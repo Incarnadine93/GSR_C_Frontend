c1901366ff36e603a1b000eb43c835e6e2aa03d5
unknown <anpham93@hotmail.com>
2023-05-17 08:54:31 -0700

    updated styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./styles/globals.css";
+import { Routes, Route } from "react-router-dom";
+import { AppContainer } from "./components/Container";
+import HomeView from "./views/HomeView";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Toaster position="bottom-right" />
+      <AppContainer>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+        </Routes>
+      </AppContainer>
+      <Footer />
     </div>
   );
 }
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,8 +1,15 @@
 const HomeView = () => {
     
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="hero min-h-screen" style={{ backgroundImage: `url("https://videogames.si.com/.image/t_share/MTk3NDQ3ODczMzM4NjgwMzI2/honkai-star-rail-bronya-1.jpg")` }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Welcome Trailblazers</h1>
+          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
